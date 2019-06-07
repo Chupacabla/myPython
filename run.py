503c3fa9412f3fec4aa573a97d74c0b7dbc2d2d3
chupacabla <chupacabla60@gmail.com>
2019-06-07 17:57:51 +0900

    削除対応＆ソースの整理

--- run.py
+++ run.py
@@ -12,16 +12,14 @@ import glob
 import os
 
 import configparser
+from datetime import datetime
+
 
 Base = declarative_base()
 
 app = Flask(__name__)
 
-ini_path = "./info.ini"
-
-
-def make_bbs_name(num):
-    return "bbs_" + str(num).zfill(8)
+INI_PATH = "./info.ini"
 
 
 class BBS_Thread_Body(Base):
@@ -29,47 +27,155 @@ class BBS_Thread_Body(Base):
 
     no = Column(Integer, primary_key=True)
     msg = Column(String(255), default=" ")
+    time_stamp = Column(String(255), default=" ")
 
     def __repr__(selef):
-        return "<BBS Thread body(no='%s', msg='%s')>" % (self.no, self.msg)
+        return "<BBS Thread body(no='%s', msg='%s',time='%s')>" % (self.no, self.msg, self.time_stamp)
 
 
-# スレ表示
+def make_bbs_name(num):
+    return "bbs_" + str(num).zfill(8)
+
+
+def make_ini_name(num):
+    base_name = make_bbs_name(num)
+    return "./bbs/"+base_name+".ini"
+
+
+def make_db_name(num):
+    base_name = make_bbs_name(num)
+    return "./bbs/"+base_name+".db"
+
+
+def make_db_connection(num):
+    return "sqlite:///" + make_db_name(num)
+
+
+def get_bbs_header():
+    value_list = []
+   # BBSヘッダの データベース確認
+    for f in glob.glob("./bbs/*.ini"):
+        # ヘッダから情報チェック
+        config = configparser.ConfigParser()
+        config.read(f)
+        value_list.append([
+            config.get("THREAD", "no"),
+            config.get("THREAD", "title"),
+        ])
+    return value_list
+
+
+# データベース削除
+def delete_thread_data(num):
+    info = get_thread_info(num)
+    try:
+        os.remove(info["ini"])
+    except PermissionError:
+        pass
+
+    try:
+        os.remove(info["db"])
+    except PermissionError:
+        pass
+
+# スレデータ用意
+
+
+def get_thread_data(num):
+    value_list = []
+    info = get_thread_info(num)
+
+    if info["isExist"] == True:
+        # データベース準備
+        engine = create_engine(info["db_connect"])
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        res = session.query(BBS_Thread_Body).all()
+        for v in res:
+            value_list.append([v.no, v.msg, v.time_stamp])
+
+    return value_list
+
+
+# スレ番号からスレッドタイトルとか情報取得
+def get_thread_info(num):
+
+    ini_name = make_ini_name(num)
+    db_name = make_db_name(num)
+    db_connect = make_db_connection(num)
+    title = ""
+    no = 0
+    isExist = False
+
+    if os.path.exists(ini_name) == True:
+
+        config = configparser.ConfigParser()
+        config.read(ini_name)
+        title = config.get("THREAD", "title")
+        no = config.get("THREAD", "no")
+        isExist = True
+
+    return {"isExist": isExist, "title": title,  "db": db_name, "db_connect": db_connect,  "ini": ini_name}
+
+
 @app.route('/view/<name>', methods=['POST', 'GET'])
-def thread_view(name=None):
+def thread_view_02(name=None):
 
-    # iniからタイトルとか拾う
-    base_name = make_bbs_name(int(name))
-    ini_name = "./bbs/"+base_name+".ini"
-    db_name = "sqlite:///bbs/"+base_name+".db"
-
-    # スレッドがあるかチェック
-    if os.path.exists(ini_name) == False:
-        return render_template('not_found.html')
-
-    config = configparser.ConfigParser()
-    config.read(ini_name)
-    title = config.get("THREAD", "title")
-
-    # データベース準備
-    engine = create_engine(db_name)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    thread_no = int(name)
+    info = get_thread_info(thread_no)
 
     # レス入力がある場合
     if request.method == "POST":
-        #        max_no = session.
-        res = request.form["res"]
-        session.add(BBS_Thread_Body(msg=res))
-        session.commit()
+        # データベース準備
+        if info["isExist"] == True:
+            engine = create_engine(info["db_connect"])
+            Base.metadata.create_all(engine)
+            Session = sessionmaker(bind=engine)
+            session = Session()
 
-    value_list = []
-    res = session.query(BBS_Thread_Body).all()
-    for v in res:
-        value_list.append([v.no, v.msg])
+            res = request.form["res"]
+            t_stamp = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+            session.add(BBS_Thread_Body(msg=res, time_stamp=t_stamp))
+            session.commit()
 
-    return render_template('view.html', value_list=value_list, title=title, thread_no=name)
+    value_list = get_thread_data(thread_no)
+    return render_template('view.html', value_list=value_list, title=info["title"], thread_no=thread_no)
+
+# スレ表示
+@app.route('/view/<name>')
+def thread_view(name=None):
+
+    thread_no = int(name)
+    value_list = get_thread_data(thread_no)
+    info = get_thread_info(thread_no)
+
+    if len(value_list) == 0:
+        return render_template('not_found.html')
+
+    return render_template('view.html', value_list=value_list, title=info["title"], thread_no=thread_no)
+
+
+# スレ削除
+@app.route('/delete_thread')
+def delete_thread():
+
+    value_list = get_bbs_header()
+    return render_template("delete_thread.html", value_list=value_list)
+
+# スレ削除
+@app.route('/delete_thread',  methods=['POST'])
+def delete_thread_02():
+
+    if request.method == "POST":
+        checks = request.form.getlist("select")
+        # スレッド削除
+        for num in checks:
+            delete_thread_data(str(num))
+        pass
+
+    value_list = get_bbs_header()
+    return render_template("index.html", value_list=value_list)
 
 
 @app.route("/make", methods=['POST', 'GET'])
@@ -81,15 +187,17 @@ def make_new_data():
 
     if request.method == "POST":
         title = request.form["title"]
+        message = request.form["message"]
+        t_stamp = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
 
         # スレ番号取得
         config = configparser.ConfigParser()
-        config.read(ini_path)
+        config.read(INI_PATH)
         thread_no = int(config.get("BBS", "MakingCounter"))
 
         base_name = make_bbs_name(thread_no)
-        ini_name = "./bbs/"+base_name+".ini"
-        db_name = "sqlite:///bbs/"+base_name+".db"
+        ini_name = make_ini_name(thread_no)
+        db_connect = make_db_connection(thread_no)
 
         # ini 生成
         config2 = configparser.ConfigParser()
@@ -101,18 +209,18 @@ def make_new_data():
             config2.write(file)
 
         # データベース生成
-        engine = create_engine(db_name)
+        engine = create_engine(db_connect)
         Base.metadata.create_all(engine)
         Session = sessionmaker(bind=engine)
         session = Session()
-        session.add(BBS_Thread_Body(msg="dummy"))
+        session.add(BBS_Thread_Body(msg=message, time_stamp=t_stamp))
         session.flush()
         session.commit()
 
         # カウンタ更新
         thread_no += 1
         config.set("BBS", "MakingCounter", str(thread_no))
-        with open(ini_path, 'w') as file:
+        with open(INI_PATH, 'w') as file:
             config.write(file)
 
     return render_template("make_threads.html")
@@ -123,23 +231,14 @@ def hello():
     value_list = []
 
     # 設定ファイル ないなら作る
-    if os.path.exists(ini_path) == False:
+    if os.path.exists(INI_PATH) == False:
         config = configparser.ConfigParser()
         config.add_section("BBS")
         config.set("BBS", "MakingCounter", str(0))
-        with open(ini_path, 'w') as file:
+        with open(INI_PATH, 'w') as file:
             config.write(file)
 
-    # BBSヘッダの データベース確認
-    for f in glob.glob("./bbs/*.ini"):
-        # ヘッダから情報チェック
-        config = configparser.ConfigParser()
-        config.read(f)
-        value_list.append([
-            config.get("THREAD", "no"),
-            config.get("THREAD", "title"),
-        ])
-        pass
+    value_list = get_bbs_header()
 
     return render_template("index.html", value_list=value_list)
 
